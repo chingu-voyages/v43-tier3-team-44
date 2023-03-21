2c8140aad432630024cc5df526c0b62c55722095
Stan <cyclinglinguist@gmail.com>
2023-03-21 17:24:26 -0500

    feat: change server to .ts

--- server/server.ts
+++ server/server.ts
@@ -1,4 +1,12 @@
 import express from "express";
+import { Configuration, OpenAIApi } from "openai";
+
+const configuration = new Configuration({
+    apiKey: process.env.OPENAI_API_KEY,
+  });
+  const openai = new OpenAIApi(configuration);
+
+
 let app = express();
 const port = 3000;
 
@@ -9,3 +17,4 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
     console.log('listening on ' + port);
 })
+
