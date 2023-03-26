5351b7bb3e686c3d3bfb164ac3f1117db1c33db8
Stan <cyclinglinguist@gmail.com>
2023-03-26 15:20:18 -0500

    feat: set up routing

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,21 @@
-import React from 'react';
-import './App.css';
-
-import Homepage from './Homepage';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import "./App.css";
+import Homepage from "./Homepage";
 
 function App() {
   return (
     <div className="App bg-vdrk-blue text-white min-h-screen flex flex-col justify-center">
-      <Homepage />
+      <Router>
+        <Routes>
+        <Route path="/" element={<Homepage />}>
+          
+        </Route>
+        <Route path="/test" element={"test"}>
+          
+        </Route>
+        </Routes>
+      </Router>
     </div>
   );
 }
