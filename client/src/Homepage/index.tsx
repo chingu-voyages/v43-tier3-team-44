8e8214c84be4ab4a3ba9d99da2cef0c126f1dd45
Stan <cyclinglinguist@gmail.com>
2023-03-31 20:10:16 -0500

    feat: increase size of main links

--- client/src/Homepage/index.tsx
+++ client/src/Homepage/index.tsx
@@ -6,8 +6,8 @@ const Homepage = () => {
         <main className="h-96 flex flex-col">
             <h1 className="font-header text-5xl text-br-red">Welcome to DM Assistant</h1>
             <div className="experiences flex flex-col mt-14">
-                <a href="#" className="font-header underline decoration-1 underline-offset-4 text-xl text-white">Generate an Encounter</a>
-                <a href="#" className="font-header underline decoration-1 underline-offset-4 text-xl text-white mt-3">Create Characters</a>
+                <a href="#" className="font-header underline decoration-1 underline-offset-4 text-3xl text-white">Generate an Encounter</a>
+                <a href="#" className="font-header underline decoration-1 underline-offset-4 text-3xl text-white mt-3">Create Characters</a>
             </div>
             <a href="#" className="mt-5 text-white">Sign Up</a>
         </main>
