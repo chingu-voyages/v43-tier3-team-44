3ca423ade8180f189795224f41ce57bf0fe2a88d
jezraelhope <jezraelhope@gmail.com>
2023-04-06 18:24:47 -0500

    feat: add monster type field

--- client/src/EncounterGeneratorPage/index.tsx
+++ client/src/EncounterGeneratorPage/index.tsx
@@ -5,29 +5,40 @@ import encounterlogo from "../assets/encountergenerator-logo.jpeg";
 const EncounterGeneratorPage = () => {
   return (
     <div className="encounter-generator h-full mx-auto my-0 flex flex-col items-center">
-      <div className="img-container w-3/5 mt-10 mb-14">
+      <div className="img-container w-3/5 mt-5 mb-12">
         <img src={encounterlogo} alt="" />
       </div>
       <h1 className="font-header decoration-1 text-4xl text-white mb-5 h-1/6">
         Monster Filter
       </h1>
-      <form action="#" className="h-2/6 w-3/5">
-        <div className="form-group mb-2">
+      <form action="#" className="w-3/5">
+        <div className="form-group mb-3">
           <label htmlFor="difficulty" className="hidden">
             Difficulty
           </label>
           <select
             name="difficulty"
             id="difficulty"
-            className="w-4/5 rounded h-10 text-vdrk-blue font-light p-2 mb-6"
+            className="w-4/5 rounded h-10 text-vdrk-blue font-light p-2"
           >
-            <option value="" disabled selected>
+            <option value="" disabled selected className="text-light-gray">
               Select Difficulty
             </option>
             <option value="easy">Easy</option>
             <option value="medium">Medium</option>
             <option value="difficult">Difficult</option>
           </select>
+        </div>
+        <div className="form-group mb-3">
+          <label htmlFor="monster-type" className="hidden">
+            Monster Type
+          </label>
+          <input
+            type="text"
+            name="monster-type"
+            className="w-4/5 rounded h-10 text-vdrk-blue font-light p-2"
+            placeholder="Monster Type"
+          />
         </div>
         <button className="bg-br-red w-5/6 h-10 rounded p-1 ">
           <span className="font-medium">Next</span>
